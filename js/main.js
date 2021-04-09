bd751f2d7426d5df7526fab687baefa44bfb07d7
sharapuz323 <82101054+sharapuz323@users.noreply.github.com>
2021-04-09 21:59:59 +0300

    Добавил контент. Настроил слайдеры.

---
+++ js/main.js
@@ -0,0 +1,37 @@
+const swiper = new Swiper('.channel-slider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 6,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.channel-button-next',
+    prevEl: '.channel-button-prev',
+  },
+});
+
+const reSwiper = new Swiper('.recommended-slider', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 3,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.recommended-button-next',
+    prevEl: '.recommended-button-prev',
+  },
+});
+
+const swiper2 = new Swiper('.channel-slider2', {
+  // Optional parameters
+  loop: true,
+  slidesPerView: 6,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.channel-button-next2',
+    prevEl: '.channel-button-prev2',
+  },
+});
+
+
